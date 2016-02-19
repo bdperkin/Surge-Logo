43c52cb8b8695cb2f405ae602bcef24ac12e12fc
Brandon Perkins <bperkins@redhat.com>
2016-02-19 14:02:00 -0500

    Initial flattened commit

---
+++ src/pl/generate_formats.pl
@@ -0,0 +1,111 @@
+#! /usr/bin/perl -w
+
+use strict;
+use warnings FATAL => 'all';
+use File::Basename;
+use File::Path;
+
+$| = 1;
+my ( $filename, $directories, $suffix ) = fileparse($0);
+my $basedir  = $directories . "../..";
+my $fmtdir   = $basedir . "/formats";
+my $srcdir   = $basedir . "/src";
+my $pngdir   = $srcdir . "/png";
+my $txtdir   = $srcdir . "/txt";
+my $ifl      = "identify_format_list_";
+my @variants = ( "Black", "White", "No" );
+
+chdir("$txtdir");
+
+if (
+    system(
+            "paste "
+          . $ifl
+          . "{Format_lc,Description}.txt | tee "
+          . $ifl
+          . "Format_and_Description.txt"
+    )
+  )
+{
+    die "Cannot create file " . $ifl . "Format_and_Description.txt: $!\n";
+} ## end if ( system( "paste " ...))
+
+if ( system( "git add " . $ifl . "Format_and_Description.txt" ) ) {
+    die "Cannot git add " . $ifl . "Format_and_Description.txt: $!\n";
+}
+
+unless (
+    system(
+            "git commit --dry-run -m \"New Format and Description List.\" "
+          . $ifl
+          . "Format_and_Description.txt"
+    )
+  )
+{
+    if (
+        system(
+                "git commit -m \"New Format and Description List.\" "
+              . $ifl
+              . "Format_and_Description.txt"
+        )
+      )
+    {
+        die "Cannot git commit " . $ifl . "Format_and_Description.txt: $!\n";
+    } ## end if ( system( "git commit -m \"New Format and Description List.\" "...))
+} ## end unless ( system( ...))
+
+unless ( open( IFL, $ifl . "Format_and_Description.txt" ) ) {
+    die "Cannot open file " . $ifl
+      . "Format_and_Description.txt for reading: $!\n";
+}
+
+while (<IFL>) {
+    chomp $_;
+    my ( $frmt, $dscrptn ) = split( /\t/, $_ );
+
+    #print $frmt . " -- " . $dscrptn . "\n";
+
+    my $frmtdir = $fmtdir . "/" . $frmt;
+    if ( !-d $frmtdir ) {
+        unless ( mkdir($frmtdir) ) {
+            die "Cannot create directory " . $frmtdir . ": $!\n";
+        }
+    }
+
+    foreach my $variant (@variants) {
+        my $srcfile = $pngdir . "/Surge-Logo-" . $variant . "-Outline.png";
+        my $frmtfile =
+          $frmtdir . "/Surge-Logo-" . $variant . "-Outline." . $frmt;
+        my $cmd = "convert " . $srcfile . " " . $frmtfile;
+        print "Running: \"$cmd\"...";
+        if ( system($cmd ) ) {
+            die "Command \"" . $cmd . "\" failed: $!\n";
+        }
+        print "done.\n";
+
+        unless ( -f $frmtfile ) {
+            die "Did not create file " . $frmtfile . ": $!\n";
+        }
+
+        unless ( system("git status | grep 'Surge-Logo-'") ) {
+            die "Extra files found!\n";
+        }
+
+        if ( system( "git add " . $frmtdir ) ) {
+            die "Cannot git add " . $frmtdir . ": $!\n";
+        }
+
+        unless (
+            system(
+                "git commit --dry-run -m \"" . $dscrptn . "\" " . $frmtdir
+            )
+          )
+        {
+            if ( system( "git commit -m \"" . $dscrptn . "\" " . $frmtdir ) ) {
+                die "Cannot git commit " . $frmtdir . ": $!\n";
+            }
+        } ## end unless ( system( "git commit --dry-run -m \""...))
+    } ## end foreach my $variant (@variants)
+} ## end while (<IFL>)
+
+close(IFL);
---
+++ src/txt/generate_text_files.sh
@@ -0,0 +1,12 @@
+#! /bin/bash -x
+
+grep ' [*,r,w,+,-][*,r,w,+,-][*,r,w,+,-]   ' identify_format_list.txt | tee identify_format_list_clean.txt
+awk '{print $1}' identify_format_list_clean.txt | tee identify_format_list_Format.txt
+awk '{print $2}' identify_format_list_clean.txt | tee identify_format_list_Module.txt
+awk '{print $3}' identify_format_list_clean.txt | tee identify_format_list_Mode.txt
+cut -c 28- identify_format_list_clean.txt | sed -e 's/^\ //g' | sed -e 's/^\ //g' | sed -e 's/^\ //g' | tee identify_format_list_Description.txt
+cat identify_format_list_Format.txt | sed -e 's/\*$//g' | tr '[:upper:]' '[:lower:]' | tee identify_format_list_Format_lc.txt
+echo -e "# Surge-Logo\nRaleigh Surge Logo\n" > ../../README.md
+paste identify_format_list_Format_lc.txt identify_format_list_Description.txt | sed -e 's/^/* /g' >> ../../README.md
+sed -i -e 's/\t/ -- /g' ../../README.md
+
