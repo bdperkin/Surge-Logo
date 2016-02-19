58b424d55240e3ac793c9fb2513ece0fa5413427
Brandon Perkins <bperkins@redhat.com>
2016-02-19 15:12:52 -0500

    Remove URLs for regular links.

--- src/txt/generate_text_files.sh
+++ src/txt/generate_text_files.sh
@@ -7,12 +7,12 @@ awk '{print $3}' identify_format_list_clean.txt | tee identify_format_list_Mode.
 cut -c 28- identify_format_list_clean.txt | sed -e 's/^\ //g' | sed -e 's/^\ //g' | sed -e 's/^\ //g' | tee identify_format_list_Description.txt
 cat identify_format_list_Format.txt | sed -e 's/\*$//g' | tr '[:upper:]' '[:lower:]' | tee identify_format_list_Format_lc.txt
 echo -e "# Surge-Logo\nRaleigh Surge Logo\n\n| Format | Description | No Outline | White Outline | Black Outline |\n| -----: | :---------- | :---: | :---: | :---: |" > ../../README.md
-paste identify_format_list_Format_lc.txt identify_format_list_Description.txt identify_format_list_Format_lc.txt identify_format_list_Format_lc.txt identify_format_list_Format_lc.txt identify_format_list_Format_lc.txt identify_format_list_Format_lc.txt identify_format_list_Format_lc.txt | sed -e 's/^/| /g' | sed -e 's/$/ |/g' >> ../../README.md
+paste identify_format_list_Format_lc.txt identify_format_list_Description.txt identify_format_list_Format_lc.txt identify_format_list_Format_lc.txt identify_format_list_Format_lc.txt identify_format_list_Format_lc.txt identify_format_list_Format_lc.txt identify_format_list_Format_lc.txt | sed -e 's/^/| /g' | sed -e 's/$/) |/g' >> ../../README.md
 sed -i -e 's/\t/ | /' ../../README.md
-sed -i -e 's/\t/ | https:\/\/raw\.githubusercontent\.com\/bdperkin\/Surge-Logo\/master\/formats\//' ../../README.md
+sed -i -e 's/\t/ | [No Outline](https:\/\/raw\.githubusercontent\.com\/bdperkin\/Surge-Logo\/master\/formats\//' ../../README.md
 sed -i -e 's/\t/\/Surge-Logo-No-Outline\./' ../../README.md
-sed -i -e 's/\t/ | https:\/\/raw\.githubusercontent\.com\/bdperkin\/Surge-Logo\/master\/formats\//' ../../README.md
+sed -i -e 's/\t/) | [White Outline](https:\/\/raw\.githubusercontent\.com\/bdperkin\/Surge-Logo\/master\/formats\//' ../../README.md
 sed -i -e 's/\t/\/Surge-Logo-White-Outline\./' ../../README.md
-sed -i -e 's/\t/ | https:\/\/raw\.githubusercontent\.com\/bdperkin\/Surge-Logo\/master\/formats\//' ../../README.md
+sed -i -e 's/\t/) | [Black Outline](https:\/\/raw\.githubusercontent\.com\/bdperkin\/Surge-Logo\/master\/formats\//' ../../README.md
 sed -i -e 's/\t/\/Surge-Logo-Black-Outline\./' ../../README.md
 
